790ff651fbb25bfc1a8a4a1e43fe3c9106a55e5f
«Daviani» <d.fillatre.pro@gmail.com>
2021-02-11 22:16:36 +0100

    Replacement of the uuid generation script by the npm uuid library

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,3 +1,5 @@
+import { v4 as uuidv4 } from 'uuid'
+
 export const actions = {
   GET_ITEMS: 'GET_ITEMS',
   ADD_TO_CART: 'ADD_TO_CART',
@@ -5,14 +7,8 @@ export const actions = {
   REMOVE_FROM_CART: 'REMOVE_FROM_CART',
   SAVE_CART: 'SAVE_CART',
 }
-//Universally unique identifier (v4)
-const uuid = () => {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    // eslint-disable-next-line no-mixed-operators
-    let r = Math.random() * 16 | 0, v = c === 'x' ? r : (r & 0x3 | 0x8);
-    return v.toString(16);
-  });
-}
+
+const uuid = uuidv4()
 
 export const addToCart = (item, quantity) => {
   return {
